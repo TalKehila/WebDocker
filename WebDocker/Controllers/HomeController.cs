e35938cfaa3acff3452984673e5bfab3490ec2d1
TalKehila <tal.kehila10@gmail.com>
2024-12-01 20:00:50 +0200

    added

--- WebDocker/Controllers/HomeController.cs
+++ WebDocker/Controllers/HomeController.cs
@@ -7,7 +7,12 @@ public class HomeController : ControllerBase
     [HttpGet("index")]
     public IActionResult Index()
     {
-        return Content("Hello from HomeController! this project on github action and work with docker \n Console.WriteLine(@\"\r\n       .\r\n      \":\"\r\n    ___:____     |\"\\/\"|\r\n  ,'        `.    \\  /\r\n  |  O        \\___/  |\r\n~^~^~^~^~^~^~^~^~^~^~^~^~\");");
+        return Content(@"
+         ____ 
+        /    \
+       |  ()  |
+        \____/
+  .--------------.
+  '==============='");
     }
 }
-
