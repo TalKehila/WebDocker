4099cd900c99d16c0fada2b3a8e1c64b08fd3fc8
TalKehila <tal.kehila10@gmail.com>
2024-12-01 18:47:12 +0200

    init

---
+++ WebDocker/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class HomeController : ControllerBase
+{
+    [HttpGet("index")]
+    public IActionResult Index()
+    {
+        return Content("Hello from HomeController!");
+    }
+}
+
---
+++ WebDocker/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebDocker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(policy =>
+                {
+
+                    policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseCors();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.MapGet("/", () => "Hello World kings");
+            app.Run();
+        }
+    }
+}
